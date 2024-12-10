b137bb2f98a065cdc836151c74143bf00b7a881d
Killian <killian.perso@hotmail.com>
2024-12-10 17:51:29 +0100

    Add: Commentary in favourite page

--- screens/FavouriteScreen.js
+++ screens/FavouriteScreen.js
@@ -1,33 +1,26 @@
 // Import pour react / react-native
-import { View, StyleSheet, Dimensions, ScrollView, TouchableOpacity, Text } from "react-native";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { updateUser } from "../reducers/users";
-
-// Import des composants
-import Header from "../components/Header";
-import PlaceCard from "../components/PlaceCard";
-
-// Récupération de la largeur de l'écran du téléphone
-const { width } = Dimensions.get("window");
+import { View, StyleSheet, Dimensions, ScrollView, TouchableOpacity, Text } from "react-native"; // Import pour react / react-native
+import { useEffect, useState } from "react"; // Import pour react
+import { useSelector } from "react-redux"; // Import pour récupérer les données du store
+import Header from "../components/Header"; // Import du composant Header.js
+import PlaceCard from "../components/PlaceCard"; // Import du composant PlaceCard.js
 
 // Création de la page Favoris
 export default function FavouriteScreen() {
-  const [placesLikedList, setPlacesList] = useState([]);
+  const [placesLikedList, setPlacesList] = useState([]); // État pour stocker la liste des lieux likés
 
   const user = useSelector((state) => state.user.value);
   console.log(user);
 
   useEffect(() => {
-    fetch(`https://roll-in-new-york-backend.vercel.app/favorites/places/${user.token}`)
+    fetch(`https://roll-in-new-york-backend.vercel.app/favorites/places/${user.token}`) // Requête pour récupérer les lieux likés
       .then((response) => response.json())
       .then((data) => {
-        setPlacesList(data.favoritePlaces);
-        console.log(data.favoritePlaces);
+        setPlacesList(data.favoritePlaces); // Stockage des lieux likés dans l'état placesLikedList
       });
   }, []);
 
-  const placesLiked = placesLikedList.map((place, i) => {
+  const placesLiked = placesLikedList.map((place, i) => { // Boucle pour afficher les lieux likés
     return (
       // Il manque la moyenne des avis venant des reviews
       <PlaceCard key={i} title={place.title} image={place.placePicture} description={place.overview} noteAverage={3} />
@@ -35,13 +28,13 @@ export default function FavouriteScreen() {
   });
 
   return (
-    <View style={styles.container}>
-      <Header title="My Favorites" showInput={false} />
-      <View style={styles.favouritesScreenContainer}>
-        <TouchableOpacity style={styles.button}>
+    <View style={styles.container}> {/* View contenant les éléments de la page */}
+      <Header title="My Favorites" showInput={false} /> {/* Affichage du composant Header */}
+      <View style={styles.favouritesScreenContainer}> {/* View contenant les différents éléments de la page */}
+        <TouchableOpacity style={styles.button}> {/* Bouton pour planifier sa journée */}
           <Text style={styles.textButton}>Plan my day !</Text>
         </TouchableOpacity>
-        <ScrollView showsVerticalScrollIndicator={false}>{placesLiked}</ScrollView>
+        <ScrollView showsVerticalScrollIndicator={false}>{placesLiked}</ScrollView> {/* Affichage des lieux likés */}
       </View>
     </View>
   );
